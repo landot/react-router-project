0282a05f74a7f7fd9491d7055db7b1665dab9646
landot <timowland@gmail.com>
2023-04-20 12:33:22 -0700

    fix: send footer to bottom of page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
 
 export default function Footer() {
     return (
-        <footer style={{fontFamily: 'Inter', backgroundColor: '#252525', padding: '25px', display: 'flex', justifyContent: 'center'}}>
+        <footer style={{
+            position: 'absolute',
+            width: '100%',
+            bottom: '0',
+            backgroundColor: '#252525', 
+            padding: '20px', 
+            display: 'flex', 
+            justifyContent: 'center'
+        }}>
             <p style={{color: '#AAAAAA', fontSize: '14px'}}>Ⓒ 2022 #VANLIFE</p>
         </footer>
     )
