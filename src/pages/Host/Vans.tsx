d13573aa40a85ed358cfe333b019a034c4e26541
landot <timowland@gmail.com>
2023-04-20 12:33:57 -0700

    feat: add host vans page

--- src/pages/Host/Vans.tsx
+++ src/pages/Host/Vans.tsx
@@ -1,5 +1,24 @@
+import { useState, useEffect } from "react";
+import ListedVan from "../../components/ListedVan";
+import { Van } from "../Vans/Vans";
+
 export default function Vans() {
+    const [hostVanData, setHostVanData] = useState<Van[]>();
+
+    useEffect(() => {
+        fetch("/api/host/vans")
+            .then(res => res.json())
+            .then(data => {
+                setHostVanData(data.vans);
+            })
+    }, [])
+
     return (
-        <p>asdfasdf host vans page</p>
+        <div>
+            <h4>Your listed vans</h4> 
+            {hostVanData && hostVanData.map(van => (
+                <ListedVan {...van}/> 
+            ))}
+        </div>
     )
 }
