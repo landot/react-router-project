cbd530702a7c57b762167452df4001b7206a2c73
landot <timowland@gmail.com>
2023-04-20 13:43:01 -0700

    feat: add WIP host van details page

--- src/components/ListedVan.tsx
+++ src/components/ListedVan.tsx
@@ -1,33 +1,41 @@
+import { Link } from "react-router-dom";
 
-export default function ListedVan(props: {name: string, price: number, imageUrl: string}) {
+export default function ListedVan(props: {name: string, price: number, imageUrl: string, id: string}) {
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            padding: '0 25px',
-            margin: '15px 25px',
-            alignItems: 'center',
-            backgroundColor: '#FFFFFF',
-            borderRadius: '5px',
+        // see if there is a better way of doing this to= to get the link working on the dashboard and vans page
+        <Link to={`/host/vans/${props.id}`} style={{
+            textDecoration: 'none',
+            color: '#161616'
         }}>
             <div style={{
                 display: 'flex',
+                justifyContent: 'space-between',
+                padding: '0 25px',
+                margin: '15px 25px',
                 alignItems: 'center',
+                backgroundColor: '#FFFFFF',
+                borderRadius: '5px',
+
             }}>
-                <img src={props.imageUrl} alt="van-image" style={{
-                    maxWidth: '65px',
-                    borderRadius: '5px',
-                    marginRight: '10px'
-                }}/>
                 <div style={{
-                    display: 'inline',
-                    float: 'right'
+                    display: 'flex',
+                    alignItems: 'center',
                 }}>
-                    <h5>{props.name}</h5>
-                    <p>${props.price} /day</p>
+                    <img src={props.imageUrl} alt="van-image" style={{
+                        maxWidth: '65px',
+                        borderRadius: '5px',
+                        marginRight: '10px'
+                    }}/>
+                    <div style={{
+                        display: 'inline',
+                        float: 'right'
+                    }}>
+                        <h5>{props.name}</h5>
+                        <p>${props.price} /day</p>
+                    </div>
                 </div>
+                <p>Edit</p>
             </div>
-            <p>Edit</p>
-        </div>
+        </Link>
     )
 }
--- src/pages/Host/VanDetail.tsx
+++ src/pages/Host/VanDetail.tsx
@@ -1,5 +1,73 @@
+import { useState, useEffect } from "react";
+import { AiOutlineLeft } from "react-icons/ai";
+import { Link, NavLink, useParams } from "react-router-dom";
+import { Van } from "../Vans/Vans";
+
 export default function VanDetail() {
+    const params = useParams();
+    const [vanData, setVanData] = useState<Van>();
+
+    useEffect(() => {
+        fetch(`/api/host/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => {
+                setVanData(data.vans[0]);
+            })
+    }, [])
+
+    console.log(vanData)
+
     return (
-        <p>asdfasdf host van detail page</p>
+        <div>
+            {/* duplicates logic in pages/Vans */}
+            <Link to='/host/vans' style={{padding: '5px'}}>
+                <span style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'center'}}>
+                    <AiOutlineLeft />
+                    <p style={{color: '#201F1D', fontSize: '16px'}}>Back to all vans</p>
+                </span>
+            </Link>
+            <p>{JSON.stringify(vanData)}</p>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                {
+                    vanData ? (
+                        <div style={{
+                            backgroundColor: '#FFFFFF',
+                            width: '100%',
+                            padding: '20px',
+                            margin: '25px'
+                        }}>
+                            <div style={{
+                                display: 'grid',
+                                gridTemplateColumns: '1fr 2fr',
+                                alignItems: 'center'
+                            }}>
+                                <img src={vanData.imageUrl} alt="van-image" style={{
+                                    width: '160px',
+                                    marginRight: '20px'
+                                }}/>
+                                <div>
+                                    <button className={`van-style ${vanData.type}`}>{vanData.type}</button>
+                                    <p><b>{vanData.name}</b></p>
+                                    <p><b>${vanData.price}</b> /day</p>
+                                </div>
+                            </div>
+                            <div className='van-detail-subheader' style={{
+                                display: 'flex'
+                            }}>
+                                <p className='host-nav'>Details</p>
+                                <p className='host-nav'>Pricing</p>
+                                <p className='host-nav'>Photos</p>
+                            </div>
+                            <div>
+                                <p>detail pricing and photos go here</p>
+                            </div>
+                        </div>
+                    ) : <p>Loading...</p>
+                }
+            </div>
+        </div>
     )
 }
